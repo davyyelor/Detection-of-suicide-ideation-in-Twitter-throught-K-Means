d08bf750939d773858d15e84a6aa57b7b74ac7c8
alberto <aarosteguig@gmail.com>
2023-10-18 12:38:18 +0200

    Mejoras en la clase KMeans para tener casi listo el algoritmo

--- Kmeans.py
+++ Kmeans.py
@@ -1,7 +1,14 @@
+from sklearn.cluster import k_means
+import numpy as np
+
 class KMeans_Clustering():
 
-    def __init__(self, n_cluster=None, initialisation_method=None):
-        pass
+    def __init__(self, n_cluster=None, initialisation_method=None, iter_max=100, p_value=1):
+        self.n_clusters = n_cluster
+        self.method = initialisation_method
+        self.iter_max = iter_max
+        self.p_value = p_value
+        #Devolver una matriz que sea un vector de vectores (n_clusters vectores que contienen las coordenadas de los centros)
 
     def minkowski_distance(self, vector1, vector2, p_value):
 
@@ -15,10 +22,40 @@ class KMeans_Clustering():
 
         
         return distance**(1/p_value)
-        
-eoe = KMeans_Clustering()
-x = [0, 5, 4, 5]
-y = [7, 6, 64, -1]
 
-distancia = eoe.minkowski_distance(x, y, -.5)
-print(distancia)
+
+    def ajustar(self, instances): #Itera el algoritmo de KMeans, calculando los centros (La matriz)
+        #Instances: 
+        # [[ valores_palabra ],
+        #  [ valores_palabra ]]
+        
+        centroids_asignados = {}
+
+        if self.method == None:
+            self.centroides = np.random.rand(self.n_clusters, instances[1].size())
+        
+        ###################################################
+        ### REASIGNACIÓN DE INSTANCIAS CON SU CENTROIDE ###
+        ###################################################
+
+        for instance_idx in range(instances[0].size()):
+            distanciaMin = float('inf')
+            for centroid_idx in range(self.centroids[0]):
+                distancia = self.minkowski_distance(instances[instance_idx], self.centroids[centroid_idx])
+                if distancia < distanciaMin:
+                    distanciaMin = distancia
+                    centroid = centroid_idx
+            centroids_asignados[centroid].append(instance_idx)
+
+
+        ###################################################
+        ###         CALCULAR NUEVOS CENTROIDES          ###
+        ###################################################
+
+        for vectores in centroids_asignados.values():
+            np.array
+
+
+
+    def calcular_clusters(self): #Etiqueta las instancias de inicialización
+        pass
