5e27c92d636ae3b9068c7e714486ebfc28b4c5a8
david <davidelorzag@gmail.com>
2023-10-30 22:55:12 +0100

    he añadido metricas a kmeans scikit learn y a la parte sin libreria, f-score,recall,precision,tasade errror, y silueta

--- KMeans_cuda.py
+++ KMeans_cuda.py
@@ -1,58 +1,73 @@
 import random
 from sklearn.cluster import k_means
 import numpy as np
-import pycuda.driver as cuda
-import pycuda.autoinit
-from pycuda.compiler import SourceModule
 from scipy.spatial import distance as sp_distance
 import matplotlib.pyplot as plt
 from scipy.cluster.hierarchy import dendrogram, linkage
 import joblib
 
-# Define the CUDA kernel
-mod = SourceModule("""
-__global__ void minkowski_distance(float *a, float *b, float *result, int n, float p) {
-    extern __shared__ float sdata[];
-
-    int idx = threadIdx.x + blockIdx.x * blockDim.x;
-    float sum = 0;
-
-    // Compute the local sum for this thread
-    if (idx < n) {
-        float diff = fabs(a[idx] - b[idx]);
-        sum += powf(diff, p);
-    }
-
-    // Load local sum into shared memory and synchronize
-    sdata[threadIdx.x] = sum;
-    __syncthreads();
-
-    // Do the reduction in shared memory
-    for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1) {
-        if (threadIdx.x < s) {
-            sdata[threadIdx.x] += sdata[threadIdx.x + s];
-        }
-        __syncthreads();
-    }
-
-    // Write the block's result to global memory
-    if (threadIdx.x == 0) {
-        atomicAdd(result, sdata[0]);
-    }
-}
-""")
-
-
 class KMeans_Clustering_CUDA():
 
-    def __init__(self, n_cluster=None, initialisation_method='random', iter_max=100, p_value=1):
+    def __init__(self, n_cluster=None, initialisation_method='random', iter_max=100, p_value=1, eType='standard'):
         self.n_clusters = n_cluster
         self.method = initialisation_method
         self.iter_max = iter_max
         self.p_value = p_value
+        self.type = eType
         # Devolver una matriz que sea un vector de vectores (n_clusters vectores que contienen las coordenadas de los centros)
 
+    def minkowski_distance(self, vector1, vector2, p_value):
+
+        def p_exp(x, y, p):
+            return abs(x - y) ** p
+
+        distance = 0
+        for idx in range(len(vector1)):
+            p1 = vector1[idx]
+            p2 = vector2[idx]
+            if p1 != p2:
+                distance += p_exp(p1, p2, p_value)
+
+        return distance ** (1 / p_value)
+
     def gpu_minkowski_distance(self, vec1, vec2, p_value):
+        import pycuda.driver as cuda
+        import pycuda.autoinit
+        from pycuda.compiler import SourceModule
+
+        mod = SourceModule("""
+            __global__ void minkowski_distance(float *a, float *b, float *result, int n, float p) {
+                extern __shared__ float sdata[];
+
+                int idx = threadIdx.x + blockIdx.x * blockDim.x;
+                float sum = 0;
+
+                // Compute the local sum for this thread
+                if (idx < n) {
+                    float diff = fabs(a[idx] - b[idx]);
+                    sum += powf(diff, p);
+                }
+
+                // Load local sum into shared memory and synchronize
+                sdata[threadIdx.x] = sum;
+                __syncthreads();
+
+                // Do the reduction in shared memory
+                for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1) {
+                    if (threadIdx.x < s) {
+                        sdata[threadIdx.x] += sdata[threadIdx.x + s];
+                    }
+                    __syncthreads();
+                }
+
+                // Write the block's result to global memory
+                if (threadIdx.x == 0) {
+                    atomicAdd(result, sdata[0]);
+                }
+            }
+            """)
+
+
         func = mod.get_function("minkowski_distance")
         block_size = (512, 1, 1)
         grid_size = ((len(vec1) + block_size[0] - 1) // block_size[0], 1, 1)
@@ -92,6 +107,10 @@ class KMeans_Clustering_CUDA():
         #  [ valores_palabra ]]
         N = instances.shape[0]
         instances = np.array(instances, dtype=np.float32)
+        if self.type == 'CUDA':
+            print("Calculando mediante GPU con CUDA")
+        else:
+            print("Calculando mediante CPU")
 
         # Matriz de centroides, filas son el índice del centroide y columnas los valores para esa componente en cada centroide
         self.centroides = random.sample(instances.tolist(),
@@ -110,8 +129,12 @@ class KMeans_Clustering_CUDA():
             for instance_idx in range(N):
                 distanciaMin = float('inf')
                 for centroid_idx in range(len(self.centroides)):
-                    distancia = self.gpu_minkowski_distance(instances[instance_idx], self.centroides[centroid_idx],
-                                                            self.p_value)
+                    if self.type == 'CUDA':
+                        distancia = self.gpu_minkowski_distance(instances[instance_idx], self.centroides[centroid_idx],
+                                                                self.p_value)
+                    else:
+                        distancia = self.minkowski_distance(instances[instance_idx], self.centroides[centroid_idx],
+                                                                self.p_value)
                     if distancia < distanciaMin:
                         distanciaMin = distancia
                         centroid = centroid_idx
@@ -186,8 +209,12 @@ class KMeans_Clustering_CUDA():
             for instance_idx in range(N):
                 distanciaMin = float('inf')
                 for centroid_idx in range(len(self.centroides)):
-                    distancia = self.gpu_minkowski_distance(instances[instance_idx], self.centroides[centroid_idx],
-                                                            self.p_value)
+                    if self.type == 'CUDA':
+                        distancia = self.gpu_minkowski_distance(instances[instance_idx], self.centroides[centroid_idx],
+                                                                self.p_value)
+                    else:
+                        distancia = self.minkowski_distance(instances[instance_idx], self.centroides[centroid_idx],
+                                                                self.p_value)
                     if distancia < distanciaMin:
                         distanciaMin = distancia
                         centroid = centroid_idx
@@ -260,19 +287,20 @@ class KMeans_Clustering_CUDA():
             print("El modelo no se ha cargado correctamente.")
             return None
 
-            # Calcular distancias y asignar instancias a centroides
-            assigned_clusters = []
-            for instance in instancias:
-                min_distance = float('inf')
-                assigned_centroid = -1
-                for centroid_idx, centroid in enumerate(self.centroides):
-                    distance = self.gpu_minkowski_distance(instance, centroid, self.p_value)
-                    if distance < min_distance:
-                        min_distance = distance
-                        assigned_centroid = centroid_idx
-                assigned_clusters.append(assigned_centroid)
+        # Calcular distancias y asignar instancias a centroides
+        print("Asignando ")
+        assigned_clusters = []
+        for instance in instancias:
+            min_distance = float('inf')
+            assigned_centroid = -1
+            for centroid_idx, centroid in enumerate(self.centroides):
+                distance = self.gpu_minkowski_distance(instance, centroid, self.p_value)
+                if distance < min_distance:
+                    min_distance = distance
+                    assigned_centroid = centroid_idx
+            assigned_clusters.append(assigned_centroid)
 
-            return assigned_clusters
+        return assigned_clusters
 
 
 
@@ -293,6 +321,3 @@ class KMeans_Clustering_CUDA():
             subregions.append(subregion)
 
         return np.array(subregions)
-
-
-
